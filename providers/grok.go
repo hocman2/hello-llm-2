bf37f10db714cd299ff3c02deee5f79603969e05
Matthis <matthishoc@gmail.com>
2026-01-29 18:23:21 +0100

    using responses api now

--- providers/grok.go
+++ providers/grok.go
@@ -5,11 +5,8 @@ import (
 )
 
 var OpenaiProviderGrok OpenaiProvider = OpenaiProvider {
-	Endpoint: "https://api.x.ai/v1/chat/completions",
-	Models: NewModelSelector("grok-4.1-non-reasoning", "grok-4.1-non-reasoning", "grok-4.1"),
+	Endpoint: "https://api.x.ai/v1/responses",
+	Models: NewModelSelector("grok-4-1-fast-non-reasoning", "grok-4-1-fast-non-reasoning", "grok-4-1-fast-reasoning"),
 	ApiKey: os.Getenv("XAI_API_KEY"),
-	WebSearchField: map[string]any {
-		"search_parameters": map[string]any{},
-	},
 	UseDeveloperRole: false,
 }
--- providers/openai.go
+++ providers/openai.go
@@ -12,12 +12,9 @@ import (
 )
 
 var OpenaiProviderOpenai OpenaiProvider = OpenaiProvider {
-	Endpoint: "https://api.openai.com/v1/chat/completions",
+	Endpoint: "https://api.openai.com/v1/responses",
 	Models: NewModelSelector("gpt-5-nano", "gpt-5-nano", "gpt-5.2"),
 	ApiKey: os.Getenv("OPENAI_API_KEY"),
-	WebSearchField: map[string]any {
-		"web_search_options": map[string]any{},
-	},
 	UseDeveloperRole: true,
 }
 
@@ -25,8 +22,6 @@ type OpenaiProvider struct {
 	Endpoint string
 	Models ModelSelector
 	ApiKey string
-	// Varies from provider to provider
-	WebSearchField map[string]any
 	// Openai uses "role":"developer" while some providers use "role":"system"
 	UseDeveloperRole bool
 }
@@ -64,13 +59,16 @@ func (p *OpenaiProvider) StartStreamingRequest(ctx context.Context, params Strea
 
 	bodyStruct := map[string]any {
 		"model": model,
-		"messages": messages,
+		"input": messages,
 		"stream": true,
 	}
 
 	if params.AllowWebSearch {
-		for k, v := range p.WebSearchField {
-			bodyStruct[k] = v
+		bodyStruct["tool_choice"] = "auto"
+		bodyStruct["tools"] = []map[string]any {
+			{
+				"type": "web_search",
+			},
 		}
 	}
 
@@ -106,25 +104,28 @@ func (p *OpenaiProvider) StartStreamingRequest(ctx context.Context, params Strea
 			return
 		}
 
+		// Highly minimal handling of responses api
 		eventData := eventRes.eventData
 		if len(eventData) > 0 {
-			if eventData == "[DONE]" {
+			var typePayload = struct{
+				Type string `json:"type"`
+			}{}
+			json.Unmarshal([]byte(eventData), &typePayload)
+
+			if eventData == "[DONE]" || typePayload.Type == "response.output_text.done" {
 				params.OnStreamingEnd(wholeContent.String())
 				return
 			}
 
-			var jsonPayload = struct{
-				Choices []struct {
-					Delta struct {
-						Content string `json:"content"`
-					} `json:"delta"`
-				} `json:"choices"`
-			}{}
-
-			json.Unmarshal([]byte(eventData), &jsonPayload)
-			if len(jsonPayload.Choices) > 0 {
-				wholeContent.WriteString(jsonPayload.Choices[0].Delta.Content)
-				params.OnChunkReceived(jsonPayload.Choices[0].Delta.Content)
+			if typePayload.Type == "response.output_text.delta" {
+				var jsonPayload = struct {
+					Delta string `json:"delta"`
+				}{}
+				json.Unmarshal([]byte(eventData), &jsonPayload)
+				wholeContent.WriteString(jsonPayload.Delta)
+				params.OnChunkReceived(jsonPayload.Delta)
+			} else {
+				//params.OnStreamingErr(errors.New(fmt.Sprintf("Unhandled event type: %s", typePayload.Type)))
 			}
 		}
 	}
